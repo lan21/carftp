28f62556940474ab7c92f78d15f6fdcfb7cdfec2
lan21 <allan.rakotoarivony@gmail.com>
2015-02-19 11:36:17 +0100

    ProcessList modifié

--- src/ftp/process/ProcessLIST.java
+++ src/ftp/process/ProcessLIST.java
@@ -2,6 +2,7 @@ package ftp.process;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFileAttributes;
@@ -26,32 +27,16 @@ public class ProcessLIST implements ProcessCommand {
 						"");
 				client.getCommandWriter().writeAnswer(partialAnswer);
 				result = "total "+fileList.length;
-				client.getDataWriter().writeBytes(result);
+				client.getDataWriter().writeBytes(result+"\r\n");
 				client.getDataWriter().flush();
 
+				String fileInfo = "";
 				for (File file : fileList) {
-					PosixFileAttributes attr = Files.readAttributes(
-							Paths.get(file.getAbsolutePath()),
-							PosixFileAttributes.class);
-					char dir = '-';
-					if (file.isDirectory())
-						dir = 'd';
-					String chmod = PosixFilePermissions.toString(Files
-							.getPosixFilePermissions(file.toPath()));
-					String owner = attr.owner().getName();
-					String group = attr.group().getName();
-					long size = file.length();
-					String lastModif = new SimpleDateFormat("MMM dd HH:mm")
-							.format(new Date(file.lastModified()));
-					String filename = file.getName();
-
-					result = dir + chmod + " " + owner + " " + group + " "
-							+ size + " " + lastModif + " " + filename + "\r\n";
-					System.out.println(result);
-					client.getDataWriter().writeBytes(result);
-					client.getDataWriter().flush();
+					fileInfo = fileInfo+getFileInformation(file)+"\r\n";
+					
 				}
-
+				client.getDataWriter().write(fileInfo.getBytes(Charset.forName("UTF-8")));
+				client.getDataWriter().flush();
 				client.getDataSocket().close();
 				return 226;
 
@@ -61,28 +46,9 @@ public class ProcessLIST implements ProcessCommand {
 		} else {
 			File file = new File(client.getCurrentDirectory() + '/' + param[1]);
 			try {
-				PosixFileAttributes attr = Files.readAttributes(
-						Paths.get(file.getAbsolutePath()),
-						PosixFileAttributes.class);
-				char dir = '-';
-				if (file.isDirectory())
-					dir = 'd';
-				String chmod = PosixFilePermissions.toString(Files
-						.getPosixFilePermissions(file.toPath()));
-				String owner = attr.owner().getName();
-				String group = attr.group().getName();
-				long size = file.length();
-				String lastModif = new SimpleDateFormat("MMM dd HH:mm")
-						.format(new Date(file.lastModified()));
-				String filename = file.getName();
-
-				String result = dir + chmod + " " + owner + " " + group + " "
-						+ size + " " + lastModif + " " + filename + "\r\n";
-				System.out.println(result);
-				client.getDataWriter().writeBytes(result);
-				client.getDataWriter().flush();
-
-				client.getDataWriter().writeBytes(result);
+				String fileInfo = getFileInformation(file)+"\r\n";
+				System.out.print(fileInfo);
+				client.getDataWriter().write(fileInfo.getBytes(Charset.forName("UTF-8")));
 				client.getDataWriter().flush();
 				client.getDataSocket().close();
 				return 226;
@@ -90,6 +56,26 @@ public class ProcessLIST implements ProcessCommand {
 				return 426;
 			}
 		}
+	}
+	
+	public String getFileInformation(File file) throws IOException{
+		PosixFileAttributes attr = Files.readAttributes(
+				Paths.get(file.getAbsolutePath()),
+				PosixFileAttributes.class);
+		char dir = '-';
+		if (file.isDirectory())
+			dir = 'd';
+		String chmod = PosixFilePermissions.toString(Files
+				.getPosixFilePermissions(file.toPath()));
+		String owner = attr.owner().getName();
+		String group = attr.group().getName();
+		long size = file.length();
+		String lastModif = new SimpleDateFormat("MMM dd HH:mm")
+				.format(new Date(file.lastModified()));
+		String filename = file.getName();
+
+		String fileInfo = String.format("%c%s %s %s %6d %s %s\n",dir,chmod,owner,group,size,lastModif,filename);
+		return fileInfo;
 	}
 
 }
