1a202b6615b6e747511eb507992679b2e8c18f13
tanguy <tanguy.fil@gmail.com>
2015-02-19 15:53:32 +0100

    Implementation de processMKD

--- src/ftp/process/ProcessMKD.java
+++ src/ftp/process/ProcessMKD.java
@@ -1,13 +1,27 @@
 package ftp.process;
 
+import java.io.File;
+
 import ftp.FTPClient;
 
+/**
+ * Class ProcessMKD
+ * Allow to make a new directory in the current directory
+ * @author Tanguy Maréchal, Allan Rakotoarivony
+ *
+ */
 public class ProcessMKD implements ProcessCommand {
 
 	@Override
 	public int process(String[] param, FTPClient client) {
-		// TODO Auto-generated method stub
-		return 212;
+		String path = client.getCurrentDirectory()+"/"+param[1];
+		File dir = new File(path);
+		if (dir.mkdir()){
+			return 212;
+		}
+		else {
+			return 550;
+		}
 	}
-
+	
 }
